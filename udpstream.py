82da67277ae432ac3df3e836b812989acfe2f039
BWieczorek <blazej.wieczorek9722@gmail.com>
2019-12-09 22:56:22 +0100

    Change field names, fix invalid call

--- udpstream.py
+++ udpstream.py
@@ -3,31 +3,30 @@ import socket
 class VideoController:
     def __init__(self):
         self.frame_id = 0
-        self.frame_buffer = bytes()
         self.transmitter = None
-        self.running = False
 
     def write(self, s):
         datagrams = self.transmitter.video_datagram_creator((self.frame_id, s))
         self.transmitter.video_send_data(datagrams)
         self.frame_id += 1
 
+
     def set_transmision_method(self, sender):
         self.transmitter = sender
 
 class UDPVideoTransmitter:
     def __init__(self, ip, port, repeat):
-        self.receiver_address = (ip, port)
+        self.dest_address = (ip, port)
         self.repeat = repeat
         self.payload = 1000
         # socket init
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def video_datagram_creator(self, rdata):
+    def video_datagram_creator(self, r_data):
         packet_length = self.payload
         datagrams = list()
-        frame_id = rdata[0]
-        msg = bytes(rdata[1])
+        frame_id = r_data[0]
+        msg = bytes(r_data[1])
         for i in range(0, int(len(msg) / packet_length) + 1):
             if packet_length * i + packet_length > len(msg):
                 if len(msg) != i * packet_length:
@@ -50,4 +49,4 @@ class UDPVideoTransmitter:
         packet_repeat = 2 if self.repeat else 1
         for datagram in datagrams:
             for n in range(0, packet_repeat):
-                self.socket.sendto(datagram, receiver_address)
+                self.socket.sendto(datagram, self.dest_address)

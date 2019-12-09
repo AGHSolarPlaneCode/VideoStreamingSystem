8815d7d85b28c5d5d807c514e28c8b75d150fa06
BWieczorek <blazej.wieczorek9722@gmail.com>
2019-12-09 22:34:04 +0100

    Add header to datagram

--- udpstream.py
+++ udpstream.py
@@ -2,42 +2,52 @@ import socket
 
 class VideoController:
     def __init__(self):
-        self.frameid = 0
+        self.frame_id = 0
         self.frame_buffer = bytes()
         self.transmitter = None
         self.running = False
 
     def write(self, s):
-        self.transmitter.send_data(self.transmitter.datagram_creator(s))
+        datagrams = self.transmitter.video_datagram_creator((self.frame_id, s))
+        self.transmitter.video_send_data(datagrams)
+        self.frame_id += 1
 
     def set_transmision_method(self, sender):
         self.transmitter = sender
 
 class UDPVideoTransmitter:
     def __init__(self, ip, port, repeat):
-        self.ipaddress = ip
-        self.port = port
+        self.receiver_address = (ip, port)
         self.repeat = repeat
         self.payload = 1000
         # socket init
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def datagram_creator(self, rdata):
-
+    def video_datagram_creator(self, rdata):
         packet_length = self.payload
-        packet_repeat = 2 if self.repeat else 1
         datagrams = list()
-        msg = bytes(rdata)
-        for n in range(0, packet_repeat):
-            for i in range(0, int(len(msg) / packet_length) + 1):
-                if packet_length * i + packet_length > len(msg):
-                    if len(msg) != i * packet_length:
-                        datagrams.append(int.to_bytes(i * packet_length, 4, 'big') + msg[i * packet_length:len(msg)])
-                else:
-                    datagrams.append(int.to_bytes(i * packet_length, 4, 'big') + msg[i * packet_length:packet_length* i + packet_length])
+        frame_id = rdata[0]
+        msg = bytes(rdata[1])
+        for i in range(0, int(len(msg) / packet_length) + 1):
+            if packet_length * i + packet_length > len(msg):
+                if len(msg) != i * packet_length:
+                    datagrams.append(
+                                    int.to_bytes(frame_id, 4, 'big') +
+                                    int.to_bytes(i * packet_length, 4, 'big') +
+                                    int.to_bytes(len(msg) - i*packet_length, 4, 'big') +
+                                    int.to_bytes(len(msg), 4, 'big') +
+                                    msg[i * packet_length:len(msg)])
+            else:
+                datagrams.append(
+                                int.to_bytes(frame_id, 4, 'big') +
+                                int.to_bytes(i * packet_length, 4, 'big') +
+                                int.to_bytes(packet_length, 4, 'big') +
+                                int.to_bytes(len(msg), 4, 'big') +
+                                msg[i * packet_length:packet_length * i + packet_length])
         return datagrams
 
-    def send_data(self, datagrams):
-
+    def video_send_data(self, datagrams):
+        packet_repeat = 2 if self.repeat else 1
         for datagram in datagrams:
-            self.socket.sendto(datagram, (self.ipaddress, self.port))
+            for n in range(0, packet_repeat):
+                self.socket.sendto(datagram, receiver_address)

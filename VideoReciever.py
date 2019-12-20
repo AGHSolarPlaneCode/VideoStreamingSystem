fcf1449146cee2b0d84acb4b1d9d01c3513a7ab0
Marcin Mucha <marcin485@op.pl>
2019-12-20 22:53:36 +0100

    Fix frame fetcher

--- VideoReciever.py
+++ VideoReciever.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 
-ADDRESS = ('localhost', 8880)
+ADDRESS = ('192.168.3.10', 8880)
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,29 +22,30 @@ except socket.error as msg:
     sys.exit()
 
 
-frame_high = bytes()
-frame_low = bytes()
+frame_high = bytearray()
+frame_low = bytearray()
 actual_frame: int = 0
 
 while True:
     d, address = s.recvfrom(1016)
+
     if int.from_bytes(d[0:4], 'big') >= actual_frame + 2:
         if actual_frame % 2 == 1:
             sendSocket.sendto(frame_high, ('localhost', 7787))
-            frame_high = bytes()
+            frame_high.clear()
         elif actual_frame % 2 == 0:
             sendSocket.sendto(frame_low, ('localhost', 7787))
-            frame_low = bytes()
+            frame_low.clear()
         actual_frame += 1
 
     if actual_frame % 2 == 1:
         if actual_frame == int.from_bytes(d[0:4], "big"):
-            frame_high[int.from_bytes(d[4:8], 'big'), int.from_bytes(d[4:8], 'big') + int.from_bytes(d[8:12], 'big')] = d[16:1016]
+            frame_high[int.from_bytes(d[4:8], 'big') : int.from_bytes(d[4:8], 'big') + int.from_bytes(d[8:12], 'big')] = d[16:1016]
         elif int.from_bytes(d[0:4], "big") > actual_frame:
-            frame_low[int.from_bytes(d[4:8], 'big'), int.from_bytes(d[4:8], 'big') + int.from_bytes(d[8:12], 'big')] = d[16:1016]
+            frame_low[int.from_bytes(d[4:8], 'big') : int.from_bytes(d[4:8], 'big') + int.from_bytes(d[8:12], 'big')] = d[16:1016]
     if actual_frame % 2 == 0:
         if actual_frame == int.from_bytes(d[0:4], "big"):
-            frame_low[int.from_bytes(d[4:8], 'big'), int.from_bytes(d[4:8], 'big') + int.from_bytes(d[8:12], 'big')] = d[16:1016]
+            frame_low[int.from_bytes(d[4:8], 'big') : int.from_bytes(d[4:8], 'big') + int.from_bytes(d[8:12], 'big')] = d[16:1016]
         elif int.from_bytes(d[0:4], "big") > actual_frame:
-            frame_high[int.from_bytes(d[4:8], 'big'), int.from_bytes(d[4:8], 'big') + int.from_bytes(d[8:12], 'big')] = d[16:1016]
+            frame_high[int.from_bytes(d[4:8], 'big'): int.from_bytes(d[4:8], 'big') + int.from_bytes(d[8:12], 'big')] = d[16:1016]
 

4804ab6ff07f811aad72bde29934f284d6629c31
Marcin Mucha <marcin485@op.pl>
2019-12-29 20:27:15 +0100

    Added VLC subprocess to video frame service

--- VideoReciever.py
+++ VideoReciever.py
@@ -1,26 +1,18 @@
 import socket
 import sys
+import subprocess
 
 ADDRESS = ('192.168.3.10', 8880)
+cmdline = ['vlc', '--demux', 'h264', '--h264-fps', '30', '-']
+
+player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sendSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-except socket.error as msg:
-    print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
-try:
-    sendSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-except socket.error as msg:
-    print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
-
-try:
     s.bind(ADDRESS)
 except socket.error as msg:
-    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    print('Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
-
 
 frame_high = bytearray()
 frame_low = bytearray()
@@ -29,15 +21,12 @@ actual_frame: int = 0
 while True:
     d, address = s.recvfrom(1016)
 
-
     if int.from_bytes(d[0:4], 'big') >= actual_frame + 2:
         if actual_frame % 2 == 0:
-            print("Frame_low is rdy")
-            #inject frame_low to pipeline here
+            player.stdin.write(frame_low)
             frame_low.clear()
         if actual_frame % 2 == 1:
-            print("Frame_high is rdy")
-            #inject frame_high to pipeline here
+            player.stdin.write(frame_high)
             frame_high.clear()
         actual_frame = int.from_bytes(d[0:4], 'big') - 1
     if actual_frame % 2 == 1:
